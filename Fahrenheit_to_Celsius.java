fedb27878f72214bfd8cef293b1646090eeb261c
keerthana sundara <112751424+Keerthana-116@users.noreply.github.com>
2022-09-11 20:29:15 +0530

    import java.util.Scanner; class Demo7 { 	public static void main(String args[]) 	{ 		int F; 		Scanner sc=new Scanner(System.in);
    		F=sc.nextInt();
    
    	float C=(float)(F-32)*5/9;
    
    		System.out.format("%.2f",C);
    	}
    }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Demo7
+{
+	public static void main(String args[])
+	{
+		int F;
+		Scanner sc=new Scanner(System.in);
+
+		F=sc.nextInt();
+
+	float C=(float)(F-32)*5/9;
+
+		System.out.format("%.2f",C);
+	}
+}
