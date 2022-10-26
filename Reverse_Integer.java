59078a96ca9e56bb48754f2277e9d81901465d68
keerthana sundara <112751424+Keerthana-116@users.noreply.github.com>
2022-10-26 22:57:42 +0530

    import java.util.Scanner;  class ReverseInteger  {      public static int findReverseInteger(int n)      {          int sum=0,r;          while(n>0||n<0)          {              r=n%10;              sum=sum*10 + r;              n=n/10;}return sum;
         }
         public static void main(String args[])
         {
             Scanner sc=new Scanner(System.in);
             int n,res;
             n=sc.nextInt();
             res=findReverseInteger(n);
             System.out.println(res);
             
         }
         
     }

--- Reverse_Integer.java
+++ Reverse_Integer.java
@@ -1,23 +1,25 @@
-import java.util.Scanner;
-class Sample
-{
-    public static int findReverse(int n)
-    {
-        int sum=0,r;
-        while(n>0||n<0)
-        {
-            r=n%10;
-            sum=sum*10+r;
-            n=n/10;
-        }
-        return sum;
-    }
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int n,res;
-        n=sc.nextInt();
-        res=findReverse(n);
-        System.out.println(res);
-    }
-}
+ import java.util.Scanner;
+ class ReverseInteger
+ {
+     public static int findReverseInteger(int n)
+     {
+         int sum=0,r;
+         while(n>0||n<0)
+         {
+             r=n%10;
+             sum=sum*10 + r;
+             n=n/10;}return sum;
+         
+     }
+     public static void main(String args[])
+     {
+         Scanner sc=new Scanner(System.in);
+         int n,res;
+         n=sc.nextInt();
+         res=findReverseInteger(n);
+         System.out.println(res);
+         
+     }
+     
+ }
+ 
